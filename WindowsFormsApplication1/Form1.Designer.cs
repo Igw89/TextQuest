61fbe54baea58ffd12877068c3b7457954b3104c
Igw89 <igw89@yandex.ru>
2018-05-30 16:54:24 +0300

    FirstUpload

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.ImeMode = System.Windows.Forms.ImeMode.On;
+            this.textBox1.Location = new System.Drawing.Point(20, 20);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(490, 240);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(20, 280);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(490, 196);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(530, 280);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(390, 196);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(530, 206);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(184, 54);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Начать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(942, 493);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //BackgroundImage = new Bitmap(@"C:\Users\777\Desktop\fon.jpg");
+        }
+        int poz1 = 0;
+        int poz2 = 0;
+        int poz3 = 0;
+        int poz4 = 0;
+        int poz5 = 0;
+        int poz6 = 0;
+
+        string [] ydot = new string [] { "21000", "12000" };
+        int gnrint = 0;
+        string delinf = "21000";
+        
+
+
+
+
+        string[][] gentx = new string[][] { };
+        string[][] genot = new string[][] { };
+        string[][] genotnaot = new string[][] { };
+
+
+        string[][] text = new string[][] {
+            new string [] { "Вывожу основной текст сюда, задаю вопрос, отвечай!" },
+            new string [] { "Вывожу второй текст" },
+            new string [] { "Вывожу третий текст" }
+        };
+
+
+        string[][] otvet = new string[][] {
+              new string [] { "Ответ 1. Удаляется", "Ответ 2. Переход", "Ответ 3. Остается" },
+              new string [] { "Ответ 1а. Переход", "Ответ 2а. Удаляется", "Ответ 3а. Остается", "Ответ 4а. Остается" }
+        };
+
+        string[][] otvetnaotvet = new string[][] {
+            new string [] { "Ответ на ответ 1", "Ответ на ответ 2 и переход на новый текст", "Ответ на ответ 3" },
+            new string [] { "Ответ на ответ 1а и переход на новый текст", "Ответ на ответ 2а", "Ответ на ответ 3а"}
+        };
+
+
+
+
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int si = listBox1.SelectedIndex;
+
+
+            if (si == -1)
+            {
+                si = 1;
+            }
+
+            if (delinf[si] == '1')
+            {
+                textBox2.Clear();
+                f_otvetnaotvet();
+            }
+
+            else if (delinf[si] == '2')
+            {
+                listBox1.Items.RemoveAt(si);
+                listBox1.ClearSelected();
+            }
+
+            else if (delinf[si] == '0')
+            {
+                textBox2.Text = textBox2.Text + '\r' + '\n' + "Остаемся!";
+            }
+        }
+
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Hide();
+            f_text();
+        }
+
+
+     
+
+        private void f_text()
+        {
+            gentx = text;
+
+            listBox1.Items.Clear();
+
+            if (poz1 > gentx.Length-1)
+            {
+                poz1 = 0;
+            }
+            textBox1.Text = gentx[poz1][poz2];
+            poz1 = poz1 + 1;
+
+            f_otvet();
+        }
+
+        private void f_otvet()
+        {
+            
+            textBox2.Text = delinf;
+
+
+           genot = otvet;
+
+            if (poz3 > genot.Length-1)
+            {
+                poz3 = 0;
+            }
+
+            for (int i = 0; i < genot[poz3].Length; i++)
+            {
+                listBox1.Items.Add(genot[poz3][i]);
+            }
+            poz3 = poz3 + 1;
+        }
+
+
+
+        private void f_otvetnaotvet()
+        {
+            genotnaot = otvetnaotvet;
+            textBox2.Text = textBox2.Text + '\r' + '\n' + "Выполнилась!";
+
+        }
+
+
+
+
+
+        //otvet2.GetLength(0); Длина массива
+        //listBox1.Items.RemoveAt(listBox1.SelectedIndex); удаление выбранного элемента
+        //listBox1.Items.Clear(); Очищаем листбокс
+        //listBox1.Items.Add(otvet2[i]); Добавить элементы в листбокс
+        //listBox1.Items.Add(genot[poz3][poz4]); Добавить элементы в листбокс
+       // ListBox.ClearSelected(); Снять выделение
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
+
